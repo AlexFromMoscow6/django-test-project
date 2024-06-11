9e37f3e81161ab697ef10da0485d2856d2b38db8
AlexFromMoscow6 <47504746+AlexFromMoscow6@users.noreply.github.com>
2024-06-12 01:24:12 +0500

    Update settings.py

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mzb*=_&vm%2ybz7u&=qxdo5w5(^9n@ev9w(_)-mnsn633loqn6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
